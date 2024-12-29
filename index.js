c76032ffd4e7f6d60201ddd8053f7eb4dc5d57f3
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-12-29 17:41:19 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1><h4>test-update</h4>");
+  res.send("<h1>Hello World!</h1><h4>test-update-2</h4>");
 });
 
 const PORT = 8080;
@@ -10,4 +10,3 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
